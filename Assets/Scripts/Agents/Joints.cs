bd32851c7f9beab6dc3f05021339da4171a4be66
KiKu25 <kristersleinerts@gmail.com>
2017-06-11 22:30:22 +0300

    Turpina Muscle

--- Assets/Scripts/Agents/Joints.cs
+++ Assets/Scripts/Agents/Joints.cs
@@ -4,5 +4,76 @@ using UnityEngine;
 
 public class Joints : MonoBehaviour {
 
-   
+    private SpringJoint2D spJoint;
+
+    public GameObject goMuscle;
+
+    public float minDist { get; set; }
+    public float maxDist { get; set; }
+    public bool spawnMuscle = true;
+
+    private void Start()
+    { 
+        spJoint = GetComponent<SpringJoint2D>();
+        SpawnMuscle(spJoint.connectedBody.GetComponent<GameObject>());
+    }
+
+    private void Update()
+    {
+        if (minDist != 0 || maxDist != 0)
+        {
+            ClampDistance(minDist, maxDist);
+        }
+    }
+
+    private void SpawnMuscle(GameObject target)
+    {
+        if (spawnMuscle)
+        {
+            Instantiate(goMuscle, new Vector3(), Quaternion.identity);
+            Muscle muscle = goMuscle.GetComponent<Muscle>();
+            muscle.target1 = gameObject;
+            muscle.target2 = target;
+        }
+    }
+
+    private void ClampDistance(float min, float max)
+    {
+        if (spJoint.distance > max)
+        {
+            spJoint.distance = max;
+        }
+        else if (spJoint.distance < min)
+        {
+            spJoint.distance = min;
+        }
+    }
+
+    private void SetDamping(float amount)
+    {
+        if (1 < amount)
+        {
+            spJoint.dampingRatio = 1;
+        }
+        if (0 > amount)
+        {
+            spJoint.dampingRatio = 0;
+        }
+        else
+        {
+            spJoint.dampingRatio = amount;
+        }
+    }
+
+    private void SetFrequency(float amount)
+    {
+        if (0 > amount)
+        {
+            spJoint.frequency = 0;
+        }
+        else
+        {
+            spJoint.frequency = amount;
+        }
+    }
 }
--- Assets/Scripts/Agents/Muscle.cs
+++ Assets/Scripts/Agents/Muscle.cs
@@ -7,6 +7,9 @@ public class Muscle : MonoBehaviour {
     public GameObject target1;
     public GameObject target2;
 
+    public float minDist { get; set; }
+    public float maxDist { get; set; }
+
     // Use this for initialization
     void Start()
     {
@@ -21,17 +24,16 @@ public class Muscle : MonoBehaviour {
     {
         transform.localScale = new Vector3(1,4 * GetDistance(target1, target2), 1);
         transform.position = new Vector3(target1.transform.position.x + (target2.transform.position.x - target1.transform.position.x) / 2, target1.transform.position.y + (target2.transform.position.y - target1.transform.position.y) / 2, target1.transform.position.z + GetDistance(target1, target2) / 2);
-        transform.eulerAngles = new Vector3(0, 0, 90 - GetAngle(new Vector3(target1.transform.position.x, target1.transform.position.y, target1.transform.position.y), new Vector3(target2.transform.position.x, target2.transform.position.y, target2.transform.position.y)));
+        transform.eulerAngles = new Vector3(0, 0, 90 - GetAngle(target1.transform.position, target2.transform.position));
     }
 
     private float GetDistance(GameObject obj1, GameObject obj2)
     {
-        return Vector3.Distance(new Vector3(obj1.transform.position.x, obj1.transform.position.y, obj1.transform.position.z), new Vector3(obj2.transform.position.x, obj2.transform.position.y, obj2.transform.position.z));
+        return Vector3.Distance(obj1.transform.position, obj2.transform.position);
     }
     
     private float GetAngle(Vector3 vec1, Vector3 vec2)
     {
-        //return Vector3.Angle(new Vector3(obj1.transform.position.x, obj1.transform.position.y, obj1.transform.position.z), new Vector3(obj2.transform.position.x, obj2.transform.position.y, obj2.transform.position.z));
         Vector3 diference = vec2 - vec1;
         float sign = (vec2.y < vec1.y) ? -1.0f : 1.0f;
         return Vector3.Angle(Vector3.left, diference) * sign;
